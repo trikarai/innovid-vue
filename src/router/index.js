b94c8f3f804676c93a56141967e1dad574b693c9
Tri S <mas.trisutrisno@gmail.com>
2019-12-26 16:52:46 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ import IncubateeAccount from "../components/Incubatee/account/IncubateeAccount";
 import IncubateeMembership from "../components/Incubatee/membership/Membership";
 import IncubateeCandidateships from "../components/Incubatee/candidate/Candidate";
 import IncubateeProfiles from "../components/Incubatee/profile/Profile";
+import IncubateeProfilesAdd from "../components/Incubatee/profile/addProfile";
 
 import TeamMember from "../components/Incubatee/asTeam/member/Member";
 import TeamMemberCandidate from "../components/Incubatee/asTeam/candidate/Candidate";
@@ -379,6 +380,14 @@ const routes = [
         component: IncubateeProfiles,
         meta: {
           level: 1,
+          requiredAuth: true, incubateeAuth: true, personnelAuth: false, sysadminAuth: false
+        }
+      },
+      {
+        path: "/incubatee/profile/:formId/add",
+        component: IncubateeProfilesAdd,
+        meta: {
+          level: 2,
           requiredAuth: true, incubateeAuth: true, personnelAuth: false, sysadminAuth: false
         }
       },
