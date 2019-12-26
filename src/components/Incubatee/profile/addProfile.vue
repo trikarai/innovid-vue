b21b42b1a518ca51bb2362a1b2f64d130edf2843
Tri S <mas.trisutrisno@gmail.com>
2019-12-26 16:52:37 +0700

    Create addProfile.vue

---
+++ src/components/Incubatee/profile/addProfile.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container grid-list-xs>
+    <v-row>
+      <v-col md="12">
+        <v-progress-circular v-if="loader" indeterminate color="primary"></v-progress-circular>
+      </v-col>
+    </v-row>
+
+    <render-form v-if="!loader" :formTemplate="formTemplate" />
+  </v-container>
+</template>
+<script>
+import bus from "@/config/bus";
+import * as config from "@/config/config";
+import auth from "@/config/auth";
+
+import RenderForm from "@/components/buildform/incubatee/renderForm";
+
+export default {
+  data() {
+    return {
+      loader: false,
+      formId: this.$route.params.formId,
+      formTemplate: {
+        id: "",
+        name: "",
+        description: "",
+        stringFields: [],
+        integerFields: [],
+        textAreaFields: [],
+        attachmentFields: [],
+        singleSelectFields: [],
+        multiSelectFields: []
+      }
+    };
+  },
+  components: {
+    RenderForm
+  },
+  mounted() {
+    this.getFormById();
+  },
+  methods: {
+    getFormById() {
+      this.loader = true;
+      this.axios
+        .get(config.baseUri + "/incubatee/profile-forms/" + this.formId, {
+          headers: auth.getAuthHeader()
+        })
+        .then(res => {
+          this.formTemplate = res.data.data;
+        })
+        .catch(res => {
+          bus.$emit("callNotif", "error", res);
+        })
+        .finally(() => {
+          this.loader = false;
+        });
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
