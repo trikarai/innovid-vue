fd379aa92b7c188088191a34383ca6ab55c0978b
Tri S <mas.trisutrisno@gmail.com>
2019-12-26 16:51:18 +0700

    add getToken  Method on auth

--- src/config/auth.js
+++ src/config/auth.js
@@ -6,6 +6,10 @@ export default {
         };
         return headers;
     },
+    getToken() {
+        let data = JSON.parse(window.localStorage.getItem('lbUser'));
+        return 'Bearer ' + data.token;
+    },
     getAuthData() {
         return window.localStorage.getItem('lbUser');
     }
