89df1fac78e4ab87c77357b4e30549c01b4dcdc4
Tri S <mas.trisutrisno@gmail.com>
2020-01-19 16:29:52 +0700

    Founder asTeamMember can manage Team Profile

---
+++ src/components/Incubatee/asTeam/teamprofile/ProfileDataList.vue
@@ -0,0 +1,223 @@
+<template>
+  <v-container grid-list-xs>
+    <v-row>
+      <v-col md="8" xs="12">
+        <v-btn color="accent" router :to="'/incubatee/team/'+teamId+'/team-profile-form'">
+          <v-icon left>add</v-icon>Add Profiles
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-show="false">
+      <v-col md="4" xs="12">
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+          clearable
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-data-table
+          :search="search"
+          :loading="tableLoad"
+          :headers="tableHeaders"
+          :items="dataList.list"
+          class="elevation-1"
+        >
+          <template v-slot:item.name="{item}">
+            <v-btn
+              class="elevation-0 mr-2"
+              fab
+              x-small
+              color="primary"
+              @click="openDetail(item.teamProfileForm.id)"
+            >
+              <v-icon>zoom_in</v-icon>
+            </v-btn>
+            {{item.teamProfileForm.name}}
+          </template>
+          <template v-slot:item.action="{item}">
+            <v-btn small color="warning" @click="leftAct(item, 'Remove')">
+              <v-icon small>delete</v-icon>
+            </v-btn>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+
+    <v-dialog v-model="dialogDelete" width="300" :persistent="true">
+      <v-card :loading="tableLoad">
+        <v-card-title>
+          <p class="text-capitalize">{{leftAction}}</p>
+        </v-card-title>
+        <v-card-text>{{leftName}}</v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn color="green" @click="deleteAccount(leftId)">Yes</v-btn>
+          <v-btn color="red" @click="dialogDelete = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      v-model="dialogDetail"
+      scrollable
+      persistent
+      :overlay="false"
+      max-width="300px"
+      transition="dialog-transition"
+    >
+      <v-card>
+        <v-card-title>
+          <p class="text-capitalize"></p>
+        </v-card-title>
+        <v-card-text v-if="loader">
+          <v-progress-linear :indeterminate="true" color="primary"></v-progress-linear>
+        </v-card-text>
+        <v-card-text>{{dataSingle}}</v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn icon color="red" @click="dialogDetail = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-container>
+</template>
+
+<script>
+import bus from "@/config/bus";
+
+import * as config from "@/config/config";
+import auth from "@/config/auth";
+
+export default {
+  data() {
+    return {
+      teamId: this.$route.params.teamId,
+      authData: "",
+      search: "",
+      dataList: { total: 0, list: [] },
+      dataSingle: { team: { name: "" }, position: "" },
+      tableLoad: false,
+      loader: false,
+      tableHeaders: [
+        { text: "Name", value: "name", sortable: false },
+        { text: "", value: "action", sortable: false, align: "right" }
+      ],
+      dialogForm: false,
+      dialogDelete: false,
+      dialogDetail: false,
+      edit: false,
+      leftId: "",
+      leftName: "",
+      leftAction: ""
+    };
+  },
+  components: {},
+  created: function() {},
+  mounted: function() {
+    this.getDataList();
+  },
+  methods: {
+    getDataList() {
+      this.tableLoad = true;
+      this.axios
+        .get(config.baseUri + "/founder/as-team-member/" + this.teamId + "/profiles", {
+          headers: auth.getAuthHeader()
+        })
+        .then(res => {
+          if (res.data.data) {
+            this.dataList = res.data.data;
+          } else {
+            this.dataList = { total: 0, list: [] };
+          }
+        })
+        .catch(() => {})
+        .finally(() => {
+          this.tableLoad = false;
+        });
+    },
+    getDataSingle(id) {
+      this.dataSingle = "";
+      this.loader = true;
+      this.axios
+        .get(
+          config.baseUri + "/founder/team/" + this.teamId + "profiles/" + id,
+          {
+            headers: auth.getAuthHeader()
+          }
+        )
+        .then(res => {
+          this.dataSingle = res.data.data;
+        })
+        .catch(res => {
+          bus.$emit("callNotif", "error", res);
+        })
+        .finally(() => {
+          this.loader = false;
+        });
+    },
+    openDetail(id) {
+      // this.dialogDetail = true;
+      // this.getDataSingle(id);
+      this.$router.push({
+        path: "/incubatee/team/" + this.teamId + "/profile/" + id
+      });
+    },
+    leftAct(item, action) {
+      this.dialogDelete = true;
+      this.leftId = item.teamProfileForm.id;
+      this.leftName = item.teamProfileForm.name;
+      this.leftAction = action;
+    },
+    deleteAccount(id) {
+      this.tableLoad = true;
+      this.axios
+        .delete(
+          config.baseUri +
+            "/founder/as-team-member/" +
+            this.teamId +
+            "/profiles/" +
+            id,
+          {
+            headers: auth.getAuthHeader()
+          }
+        )
+        .then(() => {
+          bus.$emit("callNotif", "info", "Successfully Remove Profiles Data");
+          this.refresh();
+        })
+        .catch(res => {
+          bus.$emit("callNotif", "error", res);
+        })
+        .finally(() => {
+          this.tableLoad = false;
+        });
+    },
+    refresh() {
+      this.dialogForm = false;
+      this.dialogDelete = false;
+      this.getDataList();
+    }
+  }
+};
+</script>
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for <2.1.8 */ {
+  /* transform: translateX(10px); */
+  opacity: 0;
+}
+</style>
---
+++ src/components/Incubatee/asTeam/teamprofile/addProfile.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container grid-list-xs>
+    <v-row>
+      <v-col md="12">
+        <v-progress-circular v-if="loader" indeterminate color="primary"></v-progress-circular>
+      </v-col>
+    </v-row>
+
+    <render-form v-if="!loader" :formTemplate="formTemplate" @submit-form="submitForm" />
+  </v-container>
+</template>
+<script>
+import bus from "@/config/bus";
+import * as config from "@/config/config";
+import auth from "@/config/auth";
+
+import RenderForm from "@/components/buildform/incubatee/renderForm";
+
+export default {
+  data() {
+    return {
+      loader: false,
+      formId: this.$route.params.formId,
+      teamId: this.$route.params.teamId,
+      formTemplate: {
+        id: "",
+        name: "",
+        description: "",
+        stringFields: [],
+        integerFields: [],
+        textAreaFields: [],
+        attachmentFields: [],
+        singleSelectFields: [],
+        multiSelectFields: []
+      }
+    };
+  },
+  created() {
+    window.sessionStorage.setItem("uploadMode", "founder");
+  },
+  components: {
+    RenderForm
+  },
+  mounted() {
+    this.getFormById();
+  },
+  methods: {
+    getFormById() {
+      this.loader = true;
+      this.axios
+        .get(
+          config.baseUri +
+            "/founder/as-team-member/" +
+            this.teamId +
+            "/team-profile-forms/" +
+            this.formId,
+          {
+            headers: auth.getAuthHeader()
+          }
+        )
+        .then(res => {
+          this.formTemplate = res.data.data;
+        })
+        .catch(res => {
+          bus.$emit("callNotif", "error", res);
+        })
+        .finally(() => {
+          this.loader = false;
+        });
+    },
+    submitForm(params) {
+      this.loader = true;
+      this.axios
+        .put(
+          config.baseUri +
+            "/founder/as-team-member/" +
+            this.teamId +
+            "/profiles/" +
+            this.formId,
+          params,
+          {
+            headers: auth.getAuthHeader()
+          }
+        )
+        .then(() => {
+          bus.$emit("callNotif", "success", "Form Data Uploaded");
+          this.$router.go(-2);
+        })
+        .catch(res => {
+          bus.$emit("callNotif", "error", res);
+        })
+        .finally(() => {
+          this.loader = false;
+        });
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
