f7315b83ba01aaeed4da1c6bc7d11f9fa715951f
ariefbara <arief@barapraja.com>
2020-02-03 18:32:35 +0700

    Update Dashboard.vue

--- src/components/Incubatee/dashboard/Dashboard.vue
+++ src/components/Incubatee/dashboard/Dashboard.vue
@@ -7,7 +7,7 @@
         <v-card @click="$router.push({path: '/incubatee/profile'})">
           <v-card-title style="word-break: break-word;" primary-title>
             <v-col style="max-width:100% !important" md="5">
-            <v-card color="primary" class="pa-9 elevation-5">
+            <v-card color="primary" class="pa-9 elevation-7">
               <v-icon color="#fff" x-large>folder_shared</v-icon>
             </v-card>
             </v-col>
@@ -19,10 +19,10 @@
         </v-card>
       </v-col>
       <v-col cols="12" sm="4" md="4" xs="12">
-        <v-card @click="$router.push({path: '/incubatee/profile'})">
+        <v-card @click="$router.push({path: '/incubatee/membership'})">
           <v-card-title style="word-break: break-word;" primary-title>
             <v-col style="max-width:100% !important" md="5">
-            <v-card color="primary" class="pa-9 elevation-5">
+            <v-card color="primary" class="pa-9 elevation-7">
               <v-icon color="#fff" style="width:40px;position: relative;left: 23px;" x-large>groups</v-icon>
             </v-card>
             </v-col>
@@ -40,14 +40,15 @@
       <v-col>
         <v-data-iterator :items="participationList.list" hide-default-footer :loading="tableLoad">
           <template v-slot:header>
-            <v-toolbar class="mb-2" color="indigo darken-5" dark flat dense>
+            <v-toolbar class="kastemtoolbar mb-2" color="grey darken-5" dark flat dense>
               <v-toolbar-title>Participated Program</v-toolbar-title>
             </v-toolbar>
           </template>
           <template v-slot:default="props">
             <v-row>
               <v-col v-for="item in props.items" :key="item.id" cols="12" sm="6" md="4" lg="3">
-                <v-card>
+                <v-card hover class="elevation-10">
+                  <v-img src="/img/part-program.png"></v-img>
                   <v-card-title class="subheading font-weight-bold">{{ item.program.name }}</v-card-title>
                   <v-divider></v-divider>
                   <v-list dense>
@@ -76,10 +77,11 @@
                     <v-list-item>
                       <v-list-item-content>
                         <v-btn
-                          color="red"
+                          dark
+                          color="warning"
                           router
                           :to="'/incubatee/team/' + teamId + '/participation'"
-                        >Quit</v-btn>
+                        >Quit Program</v-btn>
                       </v-list-item-content>
                     </v-list-item>
                   </v-list>
@@ -95,7 +97,7 @@
       <v-col>
         <v-data-iterator :items="registrationList.list" hide-default-footer>
           <template v-slot:header>
-            <v-toolbar class="mb-2" color="indigo darken-5" dark flat dense>
+            <v-toolbar class="kastemtoolbar mb-2" color="grey darken-5" dark flat dense>
               <v-toolbar-title>Registered Program</v-toolbar-title>
             </v-toolbar>
           </template>
@@ -113,10 +115,11 @@
                     <v-list-item>
                       <v-list-item-content>
                         <v-btn
-                          color="red"
+                          dark
+                          color="warning"
                           router
                           :to="'/incubatee/team/' + teamId + '/application'"
-                        >Cancel</v-btn>
+                        >Cancel registration</v-btn>
                       </v-list-item-content>
                     </v-list-item>
                   </v-list>
@@ -132,20 +135,50 @@
       <v-col>
         <v-data-iterator :items="availableList.list" hide-default-footer :loading="tableLoad3">
           <template v-slot:header>
-            <v-toolbar class="mb-2" color="indigo darken-5" dark flat dense>
+            <v-toolbar class="kastemtoolbar mb-2" color="grey darken-5" dark flat dense>
               <v-toolbar-title>Available Program</v-toolbar-title>
             </v-toolbar>
           </template>
+          <template v-slot:no-data>
+            <v-row class="mt-5">
+              <v-col md="2"></v-col>
+              <v-col md="4">
+                <v-img width="400" src="/img/no-data-program.png"></v-img>
+              </v-col>
+              <v-col md="4">
+                <v-card v-if="teamId == '' " style="margin-top:70px;" flat>
+                  <v-card-title>
+                    Create team first
+                  </v-card-title>
+                  <v-card-subtitle>
+                    You can join a program if you have a team, minimum with 2 members
+                  </v-card-subtitle>
+                </v-card>
+                <v-card v-else style="margin-top:70px;" flat>
+                  <v-card-title>
+                    No Program Available
+                  </v-card-title>
+                  <v-card-subtitle>
+                    You have already join all available program
+                  </v-card-subtitle>
+                </v-card>
+              </v-col>
+              <v-col md="2"></v-col>
+            </v-row>
+          </template>
           <template v-slot:default="props">
             <v-row>
-              <v-col v-for="item in props.items" :key="item.id" cols="12" sm="6" md="4" lg="3">
+              <v-col v-for="item in props.items" :key="item.id" cols="12" sm="6" md="4" lg="4">
                 <v-card>
+                  <v-img src="/img/hero-program.jpg"></v-img>
                   <v-card-title class="subheading font-weight-bold">{{ item.name }}</v-card-title>
                   <v-divider></v-divider>
                   <v-list dense>
                     <v-list-item>
-                      <v-list-item-content>Description:</v-list-item-content>
-                      <v-list-item-content class="align-end">{{ item.description }}</v-list-item-content>
+                      <v-list-item-content>Description: </v-list-item-content>
+                    </v-list-item>
+                     <v-list-item>
+                      <v-list-item-content class="grey--text">{{ item.description }}</v-list-item-content>
                     </v-list-item>
                     <v-list-item>
                       <v-list-item-content>
@@ -203,16 +236,16 @@ export default {
     }
   },
   mounted() {
-    if (this.teamId != "" && this.participationId != "") {
-      this.$router.push({
-        path:
-          "/incubatee/team/" +
-          this.teamId +
-          "/participation/" +
-          this.participationId +
-          "/mission"
-      });
-    }
+    // if (this.teamId != "" && this.participationId != "") {
+    //   this.$router.push({
+    //     path:
+    //       "/incubatee/team/" +
+    //       this.teamId +
+    //       "/participation/" +
+    //       this.participationId +
+    //       "/mission"
+    //   });
+    // }
   },
   methods: {
     getParticipant() {
@@ -294,5 +327,14 @@ export default {
 .minicard {
   position: relative;
   right: 30px;
+}
+.kastemtoolbar:before {
+  content: '';
+  position: absolute;
+  top: 0;
+  right: 0;
+  border-top: 40px solid #fafafa;
+  border-left: 40px solid rgb(158, 158, 158);
+  width: 71%;
 }
 </style>
