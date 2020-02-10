88ebeed19876f84bcd2e0749fc5f6442d68d85f8
Tri S <mas.trisutrisno@gmail.com>
2020-02-10 09:14:40 +0700

    Adding Notification on NavBar

--- src/components/Incubatee/incubateeNavBar.vue
+++ src/components/Incubatee/incubateeNavBar.vue
@@ -6,6 +6,41 @@
         <!-- <span class="font-weight-light">Inovide</span> -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <!--- notification start-->
+      <v-menu left absolute :close-on-content-click="false">
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>notifications</v-icon>
+          </v-btn>
+        </template>
+        <v-list dense>
+          <v-list-item v-for="(notif, index) in notificationList.list" :key="index">
+            <v-list-item-avatar>
+              <v-btn
+                depressed
+                color="primary"
+                x-small
+                fab
+                @click="gotoJurnal(notif.comment)"
+                v-if="notif.comment != null"
+              >
+                <v-icon small>zoom_in</v-icon>
+              </v-btn>
+              <v-btn depressed color="primary" x-small fab disabled v-else>
+                <v-icon small>zoom_in</v-icon>
+              </v-btn>
+            </v-list-item-avatar>
+            <v-list-item-content>{{notif.message}}</v-list-item-content>
+            <v-list-item-icon>
+              <v-icon color="grey" v-if="notif.read">drafts</v-icon>
+              <v-btn icon small @click="markAsRead(notif.id)" v-else>
+                <v-icon color="primary">markunread</v-icon>
+              </v-btn>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <!-- notification end-->
       <v-btn text @click="rightDrawer =! rightDrawer">
         <v-icon>settings</v-icon>
       </v-btn>
@@ -228,6 +263,7 @@ export default {
       user: "",
       teamMemberships: {},
       teamId: "",
+      notificationList: { total: 0, list: [] },
       participationList: { total: 0, list: [] },
       participationId: "",
       links: [
@@ -308,6 +344,7 @@ export default {
     if (this.teamId != "") {
       this.getParticipations();
     }
+    this.getFounderNotification();
   },
   methods: {
     goback: function() {
@@ -358,6 +395,45 @@ export default {
         .finally(() => {
           this.tableLoad = false;
         });
+    },
+    getFounderNotification() {
+      this.axios
+        .get(config.baseUri + "/founder/founder-notifications", {
+          headers: auth.getAuthHeader()
+        })
+        .then(res => {
+          this.notificationList = res.data.data;
+        })
+        .catch(() => {})
+        .finally(() => {});
+    },
+    markAsRead(id) {
+      this.axios
+        .patch(
+          config.baseUri + "/founder/founder-notifications/" + id + "/read",
+          {},
+          {
+            headers: auth.getAuthHeader()
+          }
+        )
+        .then(() => {
+          this.getFounderNotification();
+        })
+        .catch(() => {})
+        .finally(() => {});
+    },
+    gotoJurnal(comment) {
+      this.$router.push({
+        path:
+          "/incubatee/team/" +
+          comment.journal.participant.team.id +
+          "/participation/" +
+          comment.journal.participant.id +
+          "/mission/" +
+          comment.journal.participant.id +
+          "/journal/" +
+          comment.journal.id
+      });
     }
   }
 };
