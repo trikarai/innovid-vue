bb3769621b0ed327f0b642740b965d056ededc33
Tri S <mas.trisutrisno@gmail.com>
2020-02-11 15:11:11 +0700

    [patch] fix duplicate key warning on render record

--- src/components/buildform/incubatee/renderRecord.vue
+++ src/components/buildform/incubatee/renderRecord.vue
@@ -1,30 +1,18 @@
 <template>
   <div>
     <template v-for="data in fields">
-      <v-row :key="data.field.id">
+      <v-row :key="data.id">
         <b>{{data.field.name}}</b>
       </v-row>
+      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'string'">{{data.value}}</v-row>
+      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'integer'">{{data.value}}</v-row>
+      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'textarea'">{{data.value}}</v-row>
       <v-row
         class="mb-4 grey--text"
-        :key="data.field.id"
-        v-if="data.type == 'string'"
-      >{{data.value}}</v-row>
-      <v-row
-        class="mb-4 grey--text"
-        :key="data.field.id"
-        v-if="data.type == 'integer'"
-      >{{data.value}}</v-row>
-      <v-row
-        class="mb-4 grey--text"
-        :key="data.field.id"
-        v-if="data.type == 'textarea'"
-      >{{data.value}}</v-row>
-      <v-row
-        class="mb-4 grey--text"
-        :key="data.field.id"
+        :key="data.id"
         v-if="data.type == 'radio'"
       >{{data.selectedOption.name}}</v-row>
-      <v-row class="mb-4 grey--text" :key="data.field.id" v-if="data.type == 'select'">
+      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'select'">
         <template v-for="opt in data.selectedOptions">{{opt.option.name}} ,</template>
       </v-row>
     </template>
