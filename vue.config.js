6a0c7e6f8cf24a1e6f065f613aaa5f780ca7b788
Tri S <mas.trisutrisno@gmail.com>
2020-02-14 16:14:27 +0700

    Added PWA Config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+
+  pwa: {
+    name: 'Innovide',
+    themeColor: '#14DA51',
+    manifestOptions: {
+      background_color: '#2D693C'
+    },
+    msTileColor: '#121212'
+  }
 }
