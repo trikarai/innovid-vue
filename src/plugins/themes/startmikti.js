d57908a9c0f41fea1c2d8266d84809dce19ceb5b
ariefbara <arief@barapraja.com>
2020-02-14 11:34:01 +0700

    Update startmikti.js

--- src/plugins/themes/startmikti.js
+++ src/plugins/themes/startmikti.js
@@ -4,9 +4,11 @@ export default {
     light: {
         primary: '#249c90',
         sidebar: '#e4e4e4',
-        bgcolor: "#f4f3ef"
+        bgcolor: '#f4f3ef"',
+        kastemkolor: '#fff'
     },
     dark: {
-        primary: '#249c90'
+        primary: '#249c90',
+        kastemkolor: '#000'
     }
 }
