9bcf0cd789e0ecb38986bf2ae356ebbb76e456c1
Tri S <mas.trisutrisno@gmail.com>
2020-02-15 21:33:54 +0700

    Add documentation source

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,6 @@
 // This is the code piece that GenerateSW mode can't provide for us.
 // This code listens for the user's confirmation to update the app.
+// https://github.com/185driver/pwa-app-updates
 self.addEventListener('message', (e) => {
     if (!e.data) {
       return;
