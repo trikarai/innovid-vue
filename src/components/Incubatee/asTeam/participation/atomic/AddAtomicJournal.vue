feca867dd4b25c35ac2a60a015560e82aeda5edd
ariefbara <arief@barapraja.com>
2020-02-19 13:07:22 +0700

    Update AddAtomicJournal.vue

--- src/components/Incubatee/asTeam/participation/atomic/AddAtomicJournal.vue
+++ src/components/Incubatee/asTeam/participation/atomic/AddAtomicJournal.vue
@@ -62,9 +62,9 @@
       <v-col cols="12" md="12" lg="12" xs="12" v-else>
         <v-expansion-panels focusable>
           <v-expansion-panel v-for="(learning,i) in learningList.list" :key="i">
-            <v-expansion-panel-header>
+            <v-expansion-panel-header class="expand">
             <template v-slot:actions>
-              <v-chip color="black" dark>Expand<v-icon class="ml-3" color="white">keyboard_arrow_down</v-icon></v-chip>
+              <v-chip color="#505050" dark>Expand<v-icon class="ml-3" color="white">keyboard_arrow_down</v-icon></v-chip>
             </template>
               {{learning.name}}
             </v-expansion-panel-header>
@@ -405,3 +405,9 @@ export default {
   }
 };
 </script>
+
+<style>
+ .expand .v-application--is-ltr .v-expansion-panel-header__icon {
+    margin-left: 10px !important;
+}
+</style>
