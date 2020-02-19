4346346505bdcd3d0e7b32ae49483a2a94d113d8
ariefbara <arief@barapraja.com>
2020-02-19 16:49:00 +0700

    Update renderRecord.vue

--- src/components/buildform/incubatee/renderRecord.vue
+++ src/components/buildform/incubatee/renderRecord.vue
@@ -1,41 +1,75 @@
 <template>
   <div>
     <!-- {{fields}} -->
-    <template v-for="(data, index) in reOrderField(fields)">
-      <v-row :key="index">
-        <b>{{data.field.name}}</b>
-      </v-row>
-      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'string'">{{data.value}}</v-row>
-      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'integer'">{{data.value}}</v-row>
-      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'textarea'">{{data.value}}</v-row>
+    <!-- <v-switch label="label" v-model="jancux"></v-switch> -->
+    <!-- start kiri kanan-->
+    <template>
+      <template v-for="(data, index) in reOrderField(fields)">
+        <v-row :key="index">
+          <v-col md="4" lg="4" xs="12"><span class="subtitle-2 font-weight-black">{{data.field.name}}</span></v-col>
+          <v-col md="8" lg="8" xs="12" class="mb-4 grey--text" :key="data.id" v-if="data.type == 'string'">{{data.value}}</v-col>
+          <v-col md="8" lg="8" xs="12" class="mb-4 grey--text" :key="data.id" v-if="data.type == 'integer'">{{data.value}}</v-col>
+          <v-col md="8" lg="8" xs="12" class="mb-4 grey--text" :key="data.id" v-if="data.type == 'textarea'">{{data.value}}</v-col>
+          <v-col md="8" lg="8" xs="12" class="mb-4 grey--text" :key="data.id" v-if="data.type == 'radio'">
+            <template v-if="data.selectedOption != null">{{data.selectedOption.name}}</template>
+            <template v-else>-</template>
+          </v-col>
+          <v-col md="8" lg="8" xs="12" class="mb-4 grey--text" :key="data.id" v-if="data.type == 'select'">
+            <template v-if="data.selectedOptions.length == 0">-</template>
+            <template v-for="opt in data.selectedOptions">{{opt.option.name}} ,</template>
+          </v-col>
+          <v-col md="8" lg="8" xs="12" class="mb-4 grey--text" :key="data.id" v-if="data.type == 'attachment'">
+            <template v-if="data.attachedFiles.length == 0">-</template>
+            <template v-else>
+              <template v-for="file in data.attachedFiles">
+                <v-img :name="file.id" :src="base_uri+file.fileInfo.path" :key="file.id" />
+              </template>
+            </template>
+          </v-col>
+        </v-row>
+      </template>
+    </template>
+    <!-- end kiri kanan-->
 
-      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'radio'">
-        <template v-if="data.selectedOption != null">{{data.selectedOption.name}}</template>
-        <template v-else>-</template>
-      </v-row>
+    <!-- start atas bawah-->
+    <!-- <template v-if="!jancux">
+      <template v-for="(data, index) in reOrderField(fields)">
+        <v-row :key="index">
+          <b>{{data.field.name}}</b>
+        </v-row>
+        <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'string'">{{data.value}}</v-row>
+        <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'integer'">{{data.value}}</v-row>
+        <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'textarea'">{{data.value}}</v-row>
 
-      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'select'">
-        <template v-if="data.selectedOptions.length == 0">-</template>
-        <template v-for="opt in data.selectedOptions">{{opt.option.name}} ,</template>
-      </v-row>
+        <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'radio'">
+          <template v-if="data.selectedOption != null">{{data.selectedOption.name}}</template>
+          <template v-else>-</template>
+        </v-row>
 
-      <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'attachment'">
-        <template v-if="data.attachedFiles.length == 0">-</template>
-        <template v-else>
-          <template v-for="file in data.attachedFiles">
-            <!-- {{file.fileInfo.path}} -->
-            <v-img :name="file.id" :src="base_uri+file.fileInfo.path" :key="file.id" />
-          </template>
-          <!-- ajig teu kaluar wae sas
+        <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'select'">
+          <template v-if="data.selectedOptions.length == 0">-</template>
+          <template v-for="opt in data.selectedOptions">{{opt.option.name}} ,</template>
+        </v-row>
+
+        <v-row class="mb-4 grey--text" :key="data.id" v-if="data.type == 'attachment'">
+          <template v-if="data.attachedFiles.length == 0">-</template>
+          <template v-else>
+            <template v-for="file in data.attachedFiles">
+              {{file.fileInfo.path}}
+              <v-img :name="file.id" :src="base_uri+file.fileInfo.path" :key="file.id" />
+            </template>
+            ajig teu kaluar wae sas
           <img
             src="https://i.picsum.photos/id/768/85/85.jpg"
             alt="Smiley face"
             height="42"
             width="42"
-          />-->
-        </template>
-      </v-row>
-    </template>
+            />
+          </template>
+        </v-row>
+      </template>
+    </template> -->
+    <!-- end atas bawah-->
   </div>
 </template>
 <script>
@@ -47,12 +81,13 @@ export default {
   props: ["fields"],
   data() {
     return {
-      base_uri: ""
+      base_uri: "",
+      jancux: false
     };
   },
   created() {
     if (process.env.NODE_ENV === "production") {
-      this.base_uri = "https://innov.id/bara-inovasi/bara-inovasi/storage/app";
+      this.base_uri = "https://innov.id/bara-inovasi/storage/app";
     } else {
       this.base_uri = "http://localhost:8001/bara-inovasi/storage/app";
     }
