cab30666d5d4252dfa36fec1cffc99354d0ab4a0
Tri S <mas.trisutrisno@gmail.com>
2020-02-19 11:13:01 +0700

    [patch] Multiple Selection validation on min & max selected value

--- src/components/fields/type/select.vue
+++ src/components/fields/type/select.vue
@@ -2,7 +2,8 @@
   <v-col>
     <!-- build mode : {{build}} -->
     <!-- {{modeReload}} -->
-    <!-- {{field}} -->
+    <!-- Min : {{field.minValue}} -
+    Max : {{field.maxValue}}-->
     <v-select
       dense
       v-if="build"
@@ -32,12 +33,16 @@
       multiple
       persistent-hint
       outlined
+      :error-messages="errorMessages"
+      counter
+      @change="checkMinMax()"
     >
       <template v-slot:label>
         <div :class="{required : field.required}">{{field.name}}</div>
       </template>
     </v-select>
-    <!-- {{optionList}} -->
+    <template v-if="field.required">Min:{{field.minValue}} Max:{{field.maxValue}}</template>
+    <!-- {{optionList.length}} -->
   </v-col>
 </template>
 <script>
@@ -52,6 +57,7 @@ export default {
   data: function() {
     return {
       clearable: true,
+      errorMessages: [],
       optionList: []
     };
   },
@@ -80,6 +86,23 @@ export default {
         };
       }
       bus.$emit("getValue", params, this.index);
+    }
+  },
+  methods: {
+    checkMinMax() {
+      if (this.field.required) {
+        if (this.optionList.length < this.field.minValue) {
+          this.errorMessages.push(
+            "Minimal selected item is " + this.field.minValue
+          );
+        } else if (this.optionList.length > this.field.maxValue) {
+          this.errorMessages.push(
+            "Maximum selected item is " + this.field.maxValue
+          );
+        } else {
+          this.errorMessages = [];
+        }
+      }
     }
   }
 };
