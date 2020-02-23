a813a70de385d1b9fb3d7dcfc8542278bd28d8c8
Tri S <mas.trisutrisno@gmail.com>
2020-02-23 17:51:10 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,5 +5,7 @@ if (process.env.NODE_ENV === 'production') {
     // baseUri = 'http://192.168.100.2/api'
 } else {
     baseUrl = 'http://localhost:8001/api';
+    // baseUrl = 'https://innov.id/bara-inovasi/public/api';
+
 }
 export const baseUri = baseUrl;
