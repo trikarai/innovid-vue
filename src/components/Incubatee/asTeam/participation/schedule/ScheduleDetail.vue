f3c5665831c35a1e4d7dbf3f46716e7858010627
Tri S <mas.trisutrisno@gmail.com>
2020-02-26 13:44:12 +0700

    Update ScheduleDetail.vue

--- src/components/Incubatee/asTeam/participation/schedule/ScheduleDetail.vue
+++ src/components/Incubatee/asTeam/participation/schedule/ScheduleDetail.vue
@@ -39,7 +39,7 @@
             <v-card-title primary-title>
               <h3 class="headline mb-0">Report</h3>
               <v-spacer></v-spacer>
-              <v-btn color="primary" small @click="edit = !edit">
+              <v-btn color="primary" small @click="editReport">
                 <v-icon left small>edit</v-icon>Edit
               </v-btn>
             </v-card-title>
@@ -47,22 +47,27 @@
               <render-record :fields="fields" />
             </v-card-text>
           </template>
-          <template v-if="edit">
-            <v-card-title primary-title>
-              <v-spacer></v-spacer>
-              <v-btn color="warning" small @click="edit = !edit">
-                <v-icon left small>close</v-icon>Cancel Edit
-              </v-btn>
-            </v-card-title>
+          <template v-if="dataList.participantMentoringReport != null">
+            <template v-if="edit">
+              <v-card-title primary-title>
+                <v-spacer></v-spacer>
+                <v-btn color="warning" small @click="edit = !edit">
+                  <v-icon left small>close</v-icon>Cancel Edit
+                </v-btn>
+              </v-card-title>
+            </template>
           </template>
         </v-card>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12" md="6" lg="6" xs="12" v-if="edit">
-        <!-- <pre> {{dataList.participantMentoringReport}} </pre> -->
+        <pre> {{dataList.participantMentoringReport}} </pre>
+        <v-divider></v-divider>
+        <pre> {{dataList.mentoring.participantMentoringFeedbackForm}} </pre>
         <render-form
           v-if="!tableLoad"
+          :modeReload="isReload"
           :formTemplate="dataList.mentoring.participantMentoringFeedbackForm"
           @submit-form="submitForm"
         />
@@ -82,6 +87,10 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <v-overlay :value="loader">
+      <v-progress-circular color="primary" indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-container>
 </template>
 <script>
@@ -97,21 +106,25 @@ export default {
   mixins: [formDynamicMixins],
   data() {
     return {
+      isReload: false,
       search: "",
       edit: true,
       dataList: {
         id: "",
-        mentoring: { id: "", name: "" },
-        participantMentoringFeedbackForm: {
+        mentoring: {
           id: "",
           name: "",
-          description: "",
-          stringFields: [],
-          integerFields: [],
-          textAreaFields: [],
-          attachmentFields: [],
-          singleSelectFields: [],
-          multiSelectFields: []
+          participantMentoringFeedbackForm: {
+            id: "",
+            name: "",
+            description: "",
+            stringFields: [],
+            integerFields: [],
+            textAreaFields: [],
+            attachmentFields: [],
+            singleSelectFields: [],
+            multiSelectFields: []
+          }
         },
         startTime: "",
         endTime: "",
@@ -165,10 +178,13 @@ export default {
         .then(res => {
           this.dataList = res.data.data;
           if (res.data.data.participantMentoringReport !== null) {
-            this.refactorRecordJSON(res.data.data.participantMentoringReport);
             this.edit = false;
+            this.isReload = true;
+            this.refactorRecordJSON(res.data.data.participantMentoringReport);
+            // this.pairFieldValueReport(res.data.data.participantMentoringReport);
           } else {
             this.edit = true;
+            this.isReload = false;
           }
         })
         .catch(() => {})
@@ -208,11 +224,66 @@ export default {
           this.loader = false;
         });
     },
+    editReport() {
+      this.edit = true;
+    },
     leftAct(item, action) {
       this.dialogDelete = true;
       this.leftId = item.id;
       //   this.leftName = item.mentoring.name;
       this.leftAction = action;
+    },
+    pairFieldValueReport(data) {
+      //https://stackoverflow.com/questions/56444006/how-to-merge-the-property-with-same-key-in-two-object-array?noredirect=1&lq=1
+      //String
+      var map = new Map(
+        data.stringFieldRecords.map(o => [o.stringField.id, o])
+      );
+      var result = this.dataList.mentoring.participantMentoringFeedbackForm.stringFields.map(
+        o => Object.assign({}, o, map.get(o.id))
+      );
+      this.dataList.mentoring.participantMentoringFeedbackForm.stringFields = result;
+      // eslint-disable-next-line no-console
+      //textarea
+      var mapt = new Map(
+        data.textAreaFieldRecords.map(o => [o.textAreaField.id, o])
+      );
+      var resultt = this.dataList.mentoring.participantMentoringFeedbackForm.textAreaFields.map(
+        o => Object.assign({}, o, mapt.get(o.id))
+      );
+      this.dataList.mentoring.participantMentoringFeedbackForm.textAreaFields = resultt;
+      //integer
+      var mapi = new Map(
+        data.integerFieldRecords.map(o => [o.integerField.id, o])
+      );
+      var resulti = this.dataList.mentoring.participantMentoringFeedbackForm.integerFields.map(
+        o => Object.assign({}, o, mapi.get(o.id))
+      );
+      this.dataList.mentoring.participantMentoringFeedbackForm.integerFields = resulti;
+      //single select / radio
+      var mapr = new Map(
+        data.singleSelectFieldRecords.map(o => [o.singleSelectField.id, o])
+      );
+      var resultr = this.dataList.mentoring.participantMentoringFeedbackForm.singleSelectFields.map(
+        o => Object.assign({}, o, mapr.get(o.id))
+      );
+      this.dataList.mentoring.participantMentoringFeedbackForm.singleSelectFields = resultr;
+      //multi select
+      var mapm = new Map(
+        data.multiSelectFieldRecords.map(o => [o.multiSelectField.id, o])
+      );
+      var resultm = this.dataList.mentoring.participantMentoringFeedbackForm.multiSelectFields.map(
+        o => Object.assign({}, o, mapm.get(o.id))
+      );
+      this.dataList.mentoring.participantMentoringFeedbackForm.multiSelectFields = resultm;
+      //attachment
+      var mapa = new Map(
+        data.attachmentFieldRecords.map(o => [o.attachmentField.id, o])
+      );
+      var resulta = this.dataList.mentoring.participantMentoringFeedbackForm.attachmentFields.map(
+        o => Object.assign({}, o, mapa.get(o.id))
+      );
+      this.dataList.mentoring.participantMentoringFeedbackForm.attachmentFields = resulta;
     }
   }
 };
