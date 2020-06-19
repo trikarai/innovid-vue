e24db5c012861ccd17e9a0849217903a1917dd28
Tri Sutrisno <mas.trisutrisno@gmail.com>
2020-06-19 12:16:50 +0700

    show app version on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,20 @@
   <v-footer class="subtitle-1" color="#f1f1f1" height="auto" app>
     <div class="grey--text subtitle-2 ml-auto">
       &copy;2020,
-      <strong>innov.id</strong> &nbsp;&nbsp;&nbsp;&nbsp;
+      <strong>innov.id</strong> | {{appVersion}}
     </div>
   </v-footer>
 </template>
 <script>
-export default {};
+import { version } from "../../package.json";
+export default {
+  data() {
+    return {
+      appVersion: "",
+    };
+  },
+  created() {
+    this.appVersion = version;
+  },
+};
 </script>
