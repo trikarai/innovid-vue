2f9f9ac5584c03c88be8b9b87a8eb0a4294ddcc2
Tri Sutrisno <mas.trisutrisno@gmail.com>
2020-06-24 01:20:22 +0700

    Update registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
       // Routinely check for app updates by testing for a new service worker.
       setInterval(() => {
         registration.update();
-      }, 1000 * 60 * 60); // hourly checks
+      }, 1000 * 30 * 30); // 15 minutes checks
     },
     cached () {
       console.log('Content has been cached for offline use.')
