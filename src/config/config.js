d42bf974a1c2b5bcea178bce1ba09a32f41eead9
Tri Sutrisno <mas.trisutrisno@gmail.com>
2020-06-29 17:02:13 +0700

    test coomit

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === "production") {
   baseUrl = window.location.origin + "/bara-inovasi/public/api";
   // baseUrl = 'https://innov.id/bara-inovasi/public/api';
 } else {
-  var localhost = "http://localhost";
+  var localhost = "https://innov.id";
   baseUrl = localhost + "/bara-inovasi/public/api";
 }
 export const baseUri = baseUrl;
