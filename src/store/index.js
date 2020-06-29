5a2f8114061158769ef64142af46986011e62775
Tri Sutrisno <mas.trisutrisno@gmail.com>
2020-06-29 16:58:01 +0700

    [patch] Mission Detail: disable next btn for unsubmitted journal in prev mission

--- src/store/index.js
+++ src/store/index.js
@@ -6,15 +6,22 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     missionsLength: "",
+    missionsList: [],
   },
   getters: {
     getMissionLength: (state) => {
       return state.missionsLength;
     },
+    getMissionList: (state) => {
+      return state.missionsList;
+    },
   },
   mutations: {
     setMissionLength: (state, payload) => {
       state.missionsLength = payload;
+    },
+    setMissionList: (state, payload) => {
+      state.missionsList = payload;
     },
   },
   actions: {},
