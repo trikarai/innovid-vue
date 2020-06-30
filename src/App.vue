80650e402ba136856629b3e312becae83145707f
Tri Sutrisno <mas.trisutrisno@gmail.com>
2020-06-30 19:17:56 +0700

    [fix] missing PWA function

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,15 @@ export default {
     };
   },
   created() {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener("swUpdated", this.showRefreshUI, { once: true });
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      if (this.refreshing) return;
+      this.refreshing = true;
+      window.location.reload();
+    });
+
     bus.$on("callNotif", (type, res) => {
       switch (type) {
         case "error":
@@ -61,6 +70,26 @@ export default {
           break;
       }
     });
+  },
+  methods: {
+    showRefreshUI(e) {
+      // Display a snackbar inviting the user to refresh/reload the app due
+      // to an app update being available.
+      // The new service worker is installed, but not yet active.
+      // Store the ServiceWorkerRegistration instance for later use.
+      this.registration = e.detail;
+      this.snackBtnText = "Refresh";
+      this.snackWithBtnText = "New version available!";
+      this.snackWithButtons = true;
+    },
+    refreshApp() {
+      this.snackWithButtons = false;
+      // Protect against missing registration.waiting.
+      if (!this.registration || !this.registration.waiting) {
+        return;
+      }
+      this.registration.waiting.postMessage("skipWaiting");
+    },
   },
 };
 </script>
@@ -83,9 +112,9 @@ export default {
   border-radius: 100px;
   margin-top: 9px;
 }
-.v-input__slot {
-  /* background: #fff !important; */
-}
+/* .v-input__slot {
+  background: #fff !important;
+} */
 .topaccent {
   background: #00667f;
   color: #fff;
