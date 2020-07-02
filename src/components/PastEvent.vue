7d7cd4cc9b3c9e4894d667f25b7e3560afb5ca3b
ariefbara <arief@barapraja.com>
2020-07-02 18:25:18 +0700

    Create PastEvent.vue

---
+++ src/components/PastEvent.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container fluid>
+
+    <v-row>
+      <v-col cols="12" xl="9" lg="9" md="9" sm="12">
+        <v-card class="pa-5" elevation="">
+          <v-card-title class="mb-5">
+            <h4 style="color:#00667f;">Webinar Series</h4>
+          </v-card-title>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header
+                ><b
+                  >Strategi Kolaborasi Startup dgn Korporat - Arief Yahya (Didahului dgn Penjelasan MIKTI Start)</b
+                ></v-expansion-panel-header
+              >
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1593668489" src="https://cdnapisec.kaltura.com/p/2920711/sp/292071100/embedIframeJs/uiconf_id/45996371/partner_id/2920711?iframeembed=true&playerId=kaltura_player_1593668489&entry_id=0_ab43sift" width="100%" height="500" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" frameborder="0"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header
+                ><b>Dampak Pandemi pada Sektor Kuliner & Peluang Inovasi yang Muncul - Sarita Sutedja</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1593668594" src="https://cdnapisec.kaltura.com/p/2920711/sp/292071100/embedIframeJs/uiconf_id/45996371/partner_id/2920711?iframeembed=true&playerId=kaltura_player_1593668594&entry_id=0_sptxb70h" width="100%" height="500" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" frameborder="0"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+           
+          </v-expansion-panels>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
