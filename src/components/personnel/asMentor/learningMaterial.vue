aae386aa595313dba0b400233b7d1cd7ff413e52
ariefbara <arief@barapraja.com>
2020-07-22 14:15:31 +0700

    Create learningMaterial.vue

---
+++ src/components/personnel/asMentor/learningMaterial.vue
@@ -0,0 +1,113 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" xl="9" lg="9" md="9" sm="12">
+        <v-card class="pa-5" elevation="">
+          <v-card-title class="mb-5">
+            <h4 style="color:#00667f;">Materi Program</h4>
+          </v-card-title>
+          <v-expansion-panels>
+            <v-card class="container mb-5">
+            <v-card-title>1. Superhero of Me</v-card-title>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b
+                  >Superhero of Me</b
+                ></v-expansion-panel-header
+              >
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592720055" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592720055&amp;entry_id=0_1zx9nup9" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+             </v-card>
+             <v-card class="container mb-5">
+            <v-card-title>2. Area Inovasi</v-card-title>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b
+                  >Superhero of Me - Mengapa perlu?</b
+                ></v-expansion-panel-header
+              >
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592720531" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592720531&amp;entry_id=0_ky7o6opx" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b>Diagram Ekosistem</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592720609" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592720609&amp;entry_id=0_iydn3tk8" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+             </v-card>
+             <v-card class="container mb-5">
+            <v-card-title>3. Explorasi Peluang Bisnis</v-card-title>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b>Inovasi Solusi dengan SCAMPER</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592732656" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592732656&amp;entry_id=0_201fmbbn" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b>Pola Dasar Model Bisnis</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592720956" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592720956&amp;entry_id=0_vf8ldsce" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b>Evaluasi Ide Bisnis</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592727994" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592727994&amp;entry_id=0_xr1jm79l" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+             </v-card>
+             <v-card class="container mb-5">
+            <v-card-title>4. Solusi Existing</v-card-title>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b>Analisa Solusi Existing</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592800268" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592800268&amp;entry_id=0_b2cgc78f" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+             </v-card>
+              <v-card class="container mb-5">
+            <v-card-title>5. Segmen & Persona Pengguna</v-card-title>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b>Segmen Pengguna</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592800334" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592800334&amp;entry_id=0_jlc4bo36" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header class="grey--text text--darken-1"
+                ><b>Persona Pengguna</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1592800373" src="https://cdnapisec.kaltura.com/p/2884181/sp/288418100/embedIframeJs/uiconf_id/45900041/partner_id/2884181?iframeembed=true&amp;playerId=kaltura_player_1592800373&amp;entry_id=0_se7s79me" width="100%" height="500" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+              </v-card>
+
+          </v-expansion-panels>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
