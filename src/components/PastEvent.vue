02583339cd824f5f88e851da9e6a515c0db924e9
ariefbara <arief@barapraja.com>
2020-07-24 18:44:45 +0700

    Update PastEvent.vue

--- src/components/PastEvent.vue
+++ src/components/PastEvent.vue
@@ -52,6 +52,14 @@
                 <iframe id="kaltura_player_1594718611" src="https://cdnapisec.kaltura.com/p/2920711/sp/292071100/embedIframeJs/uiconf_id/45996371/partner_id/2920711?iframeembed=true&playerId=kaltura_player_1594718611&entry_id=0_olag9bo2" width="100%" height="500" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" frameborder="0"></iframe>
               </v-expansion-panel-content>
             </v-expansion-panel>
+            <v-expansion-panel>
+              <v-expansion-panel-header
+                ><b>Perubahan & Peluang Inovasi Pada Sektor Industri Perbankan - Indra Utoyo</b>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <iframe id="kaltura_player_1595579997" src="https://cdnapisec.kaltura.com/p/2920711/sp/292071100/embedIframeJs/uiconf_id/45996371/partner_id/2920711?iframeembed=true&playerId=kaltura_player_1595579997&entry_id=0_2g4q7qgg" width="100%" height="500" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" frameborder="0"></iframe>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
 
           </v-expansion-panels>
         </v-card>
