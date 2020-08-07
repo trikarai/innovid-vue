1cf1c07054c5d2d58e6c664295cbe632eb9d0143
Tri Sutrisno <mas.trisutrisno@gmail.com>
2020-08-07 17:06:07 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 let baseUrl = "";
 if (process.env.NODE_ENV === "production") {
-  baseUrl = window.location.origin + "/bara-inovasi/public/api";
-  // baseUrl = 'https://innov.id/bara-inovasi/public/api';
+  // baseUrl = window.location.origin + "/bara-inovasi/public/api";
+  baseUrl = 'https://innov.id/bara-inovasi/public/api';
 } else {
   var localhost = "http://localhost";
   baseUrl = localhost + "/bara-inovasi/public/api";
