ae248271f38e60542ab5bd44de517921e8d2cd4e
ariefbara <51996443+ariefbara@users.noreply.github.com>
2020-12-28 18:44:02 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -633,11 +633,42 @@
 <script>
 export default {
   name: "App",
-  data: function() {
+  created() {
+    document.title = "Innov.ID";
+    var s = location.hostname;
+    this.getDomain(s);
+  },
+  mounted() {
+    if (!this.isMain) {
+      this.$router.replace({ name: "mikti-home" });
+    }
+  },
+  data() {
     return {
-      title: "Home"
+      mainDomain: "",
+      isMain: false,
+      title: "Home",
     };
-  }
+  },
+  methods: {
+    getDomain(s) {
+      //s = start.mikti.id
+      s = s.replace(/^www\./, "");
+      var parts = s.split(".");
+      // ['start',''mikti, 'id']
+      // eslint-disable-next-line no-console
+      console.log("Domain parts", parts);
+      if (parts.length >= 3) {
+        this.mainDomain = parts[1];
+        this.isMain = false;
+      } else {
+        this.mainDomain = parts[0];
+        this.isMain = true;
+      }
+      //   return parts.join(".");
+      //   this.mainDomain = parts.join(".");
+    },
+  },
 };
 </script>
 
